f0d42d66a84545f4ba3330c06f760a4794f94dca
Walid Kayhal <walid.kayhal@rts.ch>
2023-03-05 11:11:51 +0100

    Add NavigationDemo issue

---
+++ NavigatonDemo/NavigationDemo/NavigationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationDemoApp.swift
+//  NavigationDemo
+//
+//  Created by Walid on 04.03.23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CountriesView()
+        }
+    }
+}
